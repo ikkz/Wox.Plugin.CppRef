28954b266f227b4ae122618432329514c6cbb63b
cildhdi <1961464399@qq.com>
2018-10-21 15:19:50 +0000

    v1.0.0

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # Wox.Plugin.CppRef
+
+一个快速查阅C/C++参考的插件
+
+![](http://img01.sogoucdn.com/app/a/100520146/da9545cb6d2326307d73a3d0b48b1c68)
+
+![](http://img01.sogoucdn.com/app/a/100520146/48f2c1bb8e0804491db6a3c742c2a814)
+
+### 安装Wox
+
+[github链接](https://github.com/Wox-launcher/Wox)
+
+
+
+###安装CppRef
+
+#### 方法一：
+
+打开wox，输入命令
+
+```
+wpm install CppRef
+```
+
+#### 方法二：
+
+到Release中下载主题包，解压到wox主题文件夹中并重启wox
---
+++ Wox.Plugin.CppRef/Main.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Wox.Plugin;
+
+namespace Wox.Plugin.CppRef
+{
+    public class Main : IPlugin
+    {
+        public void Init(PluginInitContext context)
+        {
+        }
+
+        public List<Result> Query(Query query)
+        {
+            List<Result> res = new List<Result>();
+            string[] args = query.RawQuery.Split(' ');
+            if (args.GetLength(0) > 1)
+            {
+                string url = "https://zh.cppreference.com/mwiki/index.php?search=" + args[1];
+                res.Add(new Result
+                {
+                    Title = "search " + args[1] + " in cppreference.com",
+                    IcoPath = "Images\\icon.png",
+                    Action = e =>
+                    {
+                        System.Diagnostics.Process.Start(url);
+                        return true;
+                    }
+                });
+            }
+            return res;
+        }
+    }
+}
